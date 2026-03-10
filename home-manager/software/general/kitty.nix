eb8d88d4d39da49fdc1cc3b6e1a064f8ad182d55
JTekk <joaquin@jtekk.dev>
2026-03-09 20:06:05 -0600

    fixing fonts

--- home-manager/software/general/kitty.nix
+++ home-manager/software/general/kitty.nix
@@ -4,17 +4,17 @@
   programs.kitty = {
     enable = true;
     enableGitIntegration = true;
-    #themeFile = "SyntWave84_Python";
-    #themeFile = "Dracula";
     shellIntegration.enableBashIntegration = true;
     settings = {
+
+      # --- Fonts --- #
       include = "~/.config/kitty/colors.conf";
-      # --- FONT --- #
-      font_family = "CaskaydiaCove Nerd Font Propo";
+      font_family = "CaskaydiaCove Nerd Font Mono";
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
       font_size = 10;
+      symbol_map = "U+23FB-U+23FE,U+2B58,U+E200-U+E2A9,U+E0A0-U+E0A3,U+E0B0-U+E0BF,U+E0C0-U+E0C8,U+E0CC-U+E0CF,U+E0D0-U+E0D2,U+E0D4,U+E700-U+E7C5,U+F000-U+F2E0,U+2665,U+26A1,U+F400-U+F4A8,U+F67C,U+E000-U+E00A,U+F300-U+F313,U+E5FA-U+E62B CaskaydiaCove Nerd Font";
 
       # --- CURSOR --- #
       cursor_shape = "block";
--- system/fonts.nix
+++ system/fonts.nix
@@ -1,43 +1,55 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    # Noto fonts
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    dina-font
-    proggyfonts
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      # Noto fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      dina-font
+      proggyfonts
 
-    # Nerd fonts (programming fonts with icons)
-    nerd-fonts.caskaydia-mono
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.zed-mono
-    nerd-fonts.roboto-mono
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.daddy-time-mono
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
+      # Nerd fonts (programming fonts with icons)
+      nerd-fonts.caskaydia-mono
+      nerd-fonts.caskaydia-cove
+      nerd-fonts.zed-mono
+      nerd-fonts.roboto-mono
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.daddy-time-mono
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
 
-    #X11 fonts
-    font-misc-misc
-    font-adobe-100dpi
-    font-adobe-75dpi
+      #X11 fonts
+      font-misc-misc
+      font-adobe-100dpi
+      font-adobe-75dpi
 
-    # Icon fonts
-    font-awesome
-  ];
+      # Icon fonts
+      font-awesome
+    ];
 
-  fonts.fontconfig = {
-    enable = true;
-    defaultFonts = {
-      serif = [ "Jetbrains Mono" ];
-      sansSerif = [ "CaskaydiaCove Nerd Font" ];
-      monospace = [ "CaskaydiaCove Nerd Font Mono" ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "CaskaydiaCove Nerd Font Mono"
+          "Noto Color Emoji"
+        ];
+      };
     };
   };
 }
