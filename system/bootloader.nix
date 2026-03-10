b8f8d52273d95b90b82fe6066a412b88116e5a41
JTekk <joaquin@jtekk.dev>
2026-03-09 20:51:01 -0600

    adding themes to plymouth

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,22 +1,49 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  boot.loader = {
-    limine = {
-      enable = true;
-      style = {
-        wallpapers = [ ../assets/limine/wallpaper ];
-        wallpaperStyle = "centered";
+  boot = {
+
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    loader = {
+      limine = {
+        enable = true;
+        style = {
+          wallpapers = [ ../assets/limine/wallpaper ];
+          wallpaperStyle = "centered";
+        };
+        extraConfig = ''
+          TIMEOUT=1
+        '';
       };
-      extraConfig = ''
-        TIMEOUT=0
-      '';
+
+      efi.canTouchEfiVariables = true;
     };
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.plymouth = {
-    enable = true;
-    logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
-    theme = "solar";
+
+    plymouth = {
+      enable = true;
+      logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
+      theme = "darth_vader";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [
+            "darth_vader"
+            "optimus"
+            "tech_a"
+            "tech_b"
+          ];
+        })
+      ];
+    };
   };
 }
