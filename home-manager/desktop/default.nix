516898535db79714d515f5d6cdddf5ebd4b1bcde
JTekk <joaquin@jtekk.dev>
2026-03-13 09:37:56 -0600

    splitting up desktop software and settings

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -2,23 +2,10 @@
 
 {
   imports = [
-    ./btop.nix
-    ./dconf.nix
     ./desktop-shell.nix
-    ./extras.nix
-    ./gtk-qt-theming.nix
-    ./icons.nix
-    ./kanshi.nix
     ./mango.nix
     ./mango-settings
-    ./superfile.nix
-    ./sway-audio-idle-inhibit.nix
-    ./swayidle.nix
-    ./swaylock.nix
-    ./swayosd.nix
-    ./waybar.nix
-    ./wlogout.nix
-    ./wofi.nix
-    ./xdg.nix
+    ./settings
+    ./software
   ];
 }
---
+++ home-manager/desktop/software/default.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  imports = [
+    ./btop.nix
+    ./extras.nix
+    ./kanshi.nix
+    ./superfile.nix
+    ./sway-audio-idle-inhibit.nix
+    ./swayidle.nix
+    ./swaylock.nix
+    ./swayosd.nix
+    ./waybar.nix
+    ./wlogout.nix
+    ./wofi.nix
+  ];
+}
