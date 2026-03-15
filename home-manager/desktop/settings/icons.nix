1b0a2ff6e46edcf3068c554fa899f537baa225f2
JTekk <joaquin@jtekk.dev>
2026-03-14 19:10:02 -0600

    adding themeswticher

--- home-manager/desktop/settings/icons.nix
+++ home-manager/desktop/settings/icons.nix
@@ -2,12 +2,12 @@
 
 {
   home.file.".icons" = {
-    source = ./../../../assets/icons/mouse-themes;
+    source = ../../../../assets/icons/mouse-themes;
     recursive = true;
   };
 
   home.file.".local/share/icons" = {
-    source = ./../../../assets/icons/app-icons;
+    source = ../../../../assets/icons/app-icons;
     recursive = true;
   };
 }
