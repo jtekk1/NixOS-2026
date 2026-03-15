5f64041a3d37b81b9264292f43ccee11dec0408f
JTekk <joaquin@jtekk.dev>
2026-03-14 20:22:27 -0600

    adding Sandbox to the config

---
+++ hosts/sandbox/configuration.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.nix
+    ./../../system/desktop
+    ./../../software
+    ./../../hardware/btop-intel.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "sandbox";
+}
---
+++ hosts/sandbox/disko.nix
@@ -0,0 +1,132 @@
+{ ... }:
+
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0022"
+                  "dmask=0022"
+                ];
+              };
+            };
+            luks = {
+              end = "-4G"
+              content = {
+                type = "luks";
+                name = "crypted";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "@" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd:1"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@tmp" = {
+                      mountpoint = "/var/tmp";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@cache" = {
+                      mountpoint = "/var/cache";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@snapshot" = {
+                      mountpoint = "/.snapshot";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                    "@hm-snapshot" = {
+                      mountpoint = "/home/.snapshot";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "ssd"
+                        "space_cache=v2"
+                        "discard=async"
+                      ];
+                    };
+                  };
+                };
+              };
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true;
+                randomEncryption = true;
+              };
+            }
+          };
+        };
+      };
+    };
+  };
+}
